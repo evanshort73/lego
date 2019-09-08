c832386d0c2eb7db94bf33d2cf942c01c72765a3
Eva Short <evashort73@gmail.com>
2019-09-07 21:27:42 -0700

    add Point parser demo

--- src/main.elm
+++ src/main.elm
@@ -1,7 +1,8 @@
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
-
+import Html exposing (Html, Attribute, button, div, textarea, text)
+import Html.Events exposing (onClick, onInput)
+import SQLParser
+import Html.Attributes exposing (..)
 
 main =
   Browser.sandbox { init = init, update = update, view = view }
@@ -9,25 +10,33 @@ main =
 
 -- MODEL
 
-type alias Model = Int
+type alias Model =
+  { num : Int
+  , content : String
+  }
 
 init : Model
 init =
-  0
+  { num = 0
+  , content = "(2, 3)"
+  }
 
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+type Msg = Increment | Decrement | Change String
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Increment ->
-      model + 1
+      { model | num = model.num + 1 }
 
     Decrement ->
-      model - 1
+      { model | num = model.num - 1 }
+
+    Change newContent ->
+      { model | content = newContent }
 
 
 -- VIEW
@@ -36,6 +45,14 @@ view : Model -> Html Msg
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
+    , div [] [ text (String.fromInt model.num) ]
     , button [ onClick Increment ] [ text "+" ]
+    , textarea [ placeholder "Text to reverse", value model.content, onInput Change ] []
+    , div [] [ text (
+      case SQLParser.parse model.content of
+        Ok p ->
+          Debug.toString p
+        Err deadEnds ->
+          Debug.toString deadEnds
+    ) ]
     ]
